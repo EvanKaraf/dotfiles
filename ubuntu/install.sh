c5f69ac07ea9479a5bed92a3f3b477f5876d4811
EvanKaraf <vaggeliskaraf@gmail.com>
2019-04-24 05:05:20 +0300

    Add OpenSubtitltesDownload

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -14,3 +14,11 @@ sudo apt-get --assume-yes install ruby-full 1> /dev/null
 sudo gem install fusuma 1> /dev/null
 mkdir -p $HOME/.config/fusuma
 ln -sf  $DOTFILES_ROOT/ubuntu/config.yml $HOME/.config/fusuma/config.yml
+
+echo "› Installing OpenSubtitlesDownload"
+mkdir -p ~/.local/share/nautilus/scripts/
+cd ~/.local/share/nautilus/scripts/
+wget -q https://raw.githubusercontent.com/emericg/OpenSubtitlesDownload/master/OpenSubtitlesDownload.py 1> /dev/null
+chmod u+x OpenSubtitlesDownload.py
+sed -i -r s/\'eng\'/\'eng,ell\'/ OpenSubtitlesDownload.py
+cd $DOTFILES_ROOT
